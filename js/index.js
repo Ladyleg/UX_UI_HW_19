fe33c32e39f5e40ed7d04f148346c27fbeeffe29
ladyleggett3 <78886065+ladyleggett3@users.noreply.github.com>
2021-07-07 16:36:49 -0700

    Ui interactions

--- js/index.js
+++ js/index.js
@@ -1,4 +1,98 @@
 console.log("Your index.js file is loaded correctly!");
-$('#resume').on("click", function(){
-    $('.fadeMe').fadeToggle("slow");
+$("#resume").on("click", function(){
+    $(".fadeMe").animate("slow");
 });
+$("#resume").on({
+    mouseenter: function(){
+      $(this).css("background-color", "lightgray");
+    },
+    mouseleave: function(){
+      $(this).css("background-color", "#373738");
+    },
+    click: function(){
+      $(this).css("background-color", "blanchedalmond");
+    }
+  });
+  $("#cats").on({
+    mouseenter: function(){
+      $(this).css("background-color", "lightgray");
+    },
+    mouseleave: function(){
+      $(this).css("background-color", "#373738");
+    },
+    click: function(){
+      $(this).css("background-color", "blanchedalmond");
+    }
+  });
+  $("#travelers").on({
+    mouseenter: function(){
+      $(this).css("background-color", "lightgray");
+    },
+    mouseleave: function(){
+      $(this).css("background-color", "#373738");
+    },
+    click: function(){
+      $(this).css("background-color", "blanchedalmond");
+    }
+  }); $("#treasury").on({
+    mouseenter: function(){
+      $(this).css("background-color", "lightgray");
+    },
+    mouseleave: function(){
+      $(this).css("background-color", "#373738");
+    },
+    click: function(){
+      $(this).css("background-color", "blanchedalmond");
+    }
+  });
+  var textWrapper = document.querySelector('.lady');
+  textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  
+  anime.timeline({loop: true})
+    .add({
+      targets: '.lady .letter',
+      scale: [4,1],
+      opacity: [0,1],
+      translateZ: 0,
+      easing: "easeOutExpo",
+      duration: 950,
+      delay: (el, i) => 70*i
+    }).add({
+      targets: '.lady',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
+    var textWrapper = document.querySelector('.heading .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.heading .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.heading .line',
+    translateX: [0, document.querySelector('.heading .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.heading .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.heading',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+  
